1ea085302f15675133e8695a1006749630ea057e
Richard Lang <richard.lang@activenavigation.com>
2019-09-06 13:53:01 +0100

    Skeleton Unit Tests

---
+++ PGShoppingBasket.Test/BasketTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PGShoppingBasket.Test
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        // Scenario: Basket 1
+        [Test]
+        public void GivenItemsInBasket_WhenApplyGiftVoucher_ThenTotalIsCorrect()
+        {
+            Assert.Fail();
+        }
+
+        // Scenario: Basket 2
+        [Test]
+        public void GivenItemsInBasket_WhenApplyOfferVoucherNotReachingThreshold_ThenTotalIsCorrectAndMessageDisplayed()
+        {
+            Assert.Fail();
+        }
+
+        // Scenario: Basket 3
+        [Test]
+        public void GivenItemsInBasket_WhenApplyOfferVoucherReachingThreshold_ThenDiscountDoesNotExceedQualifyingItemPriceAndTotalIsCorrect()
+        {
+            Assert.Fail();
+        }
+
+        // Scenario: Basket 4
+        [Test]
+        public void GivenItemsInBasket_WhenApplyGiftVoucherAndApplyOfferVoucher_ThenTotalIsCorrect()
+        {
+            Assert.Fail();
+        }
+
+        // Scenario: Basket 5
+        [Test]
+        public void GivenItemsInBasketIncludingGiftVoucher_WhenApplyOfferVoucherNotReachingThreshold_ThenTotalIsCorrectAndMessageDisplayed()
+        {
+            Assert.Fail();
+        }
+    }
+}
